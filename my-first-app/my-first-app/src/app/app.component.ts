21bc3a1a5488a5cc506b89543602aebb52016eb8
adoniasvsbarros <adoniasvicente@hotmail.com>
2019-06-02 20:18:54 -0300

    data binding

--- my-first-app/my-first-app/src/app/app.component.ts
+++ my-first-app/my-first-app/src/app/app.component.ts
@@ -2,8 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  //selector: [app-root], //selecting by attribute ex: <div app-root>
+  //selector: '.app-root', //selecting by class ex: <div class='app-root'>
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  /*styles: [`
+    h1 {
+      color: dodgerblue;
+    }
+  `]*/
 })
 export class AppComponent {
   name = 'Adonias';
