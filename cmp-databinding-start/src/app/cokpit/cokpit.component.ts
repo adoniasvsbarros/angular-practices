3498ffee3be9cd14c862b8003b7f96fadfc52316
adoniasvsbarros <adoniasvicente@hotmail.com>
2019-07-12 19:06:41 -0300

    adding custom property and event binding

---
+++ cmp-databinding-start/src/app/cokpit/cokpit.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-cokpit',
+  templateUrl: './cokpit.component.html',
+  styleUrls: ['./cokpit.component.css']
+})
+export class CokpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{blueprintName: string, blueprintContent: string}>();
+
+  newServerName = '';
+  newServerContent = '';
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddServer() {
+    this.serverCreated.emit({
+      serverName: this.newServerName, 
+      serverContent: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.blueprintCreated.emit({
+      blueprintName: this.newServerName,
+      blueprintContent: this.newServerContent
+    });
+  }
+
+}
