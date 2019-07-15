3141aada25af6f5369c09e4eac9359fd1500d977
adoniasvsbarros <adoniasvicente@hotmail.com>
2019-07-15 19:54:28 -0300

    getting access to ng-content with content child

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy, ViewChild, ElementRef, ContentChild } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -11,6 +11,7 @@ export class ServerElementComponent implements OnInit, OnChanges, DoCheck, After
   @Input('srvElement') element: {type: string, name: string, content: string};
   @Input() name: string;
   @ViewChild('heading', {static: true}) header: ElementRef;
+  @ContentChild('contentParagraph', {static: true}) paragraph : ElementRef;
 
   constructor() { 
     console.log('constructor called')
@@ -24,6 +25,7 @@ export class ServerElementComponent implements OnInit, OnChanges, DoCheck, After
   ngOnInit() {
     console.log('ngOnInit called');
     console.log('text content: ' + this.header.nativeElement.textContent);
+    console.log('text content of paragraph ' + this.paragraph.nativeElement.textContent);
   }
 
   ngDoCheck() {
@@ -31,7 +33,8 @@ export class ServerElementComponent implements OnInit, OnChanges, DoCheck, After
   }
 
   ngAfterContentInit(){
-    console.log('ngAfterContentInit called')
+    console.log('ngAfterContentInit called');
+    console.log('text content of paragraph ' + this.paragraph.nativeElement.textContent);
   }
 
   ngAfterContentChecked(){
