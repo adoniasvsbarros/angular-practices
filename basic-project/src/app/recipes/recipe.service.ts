3d70c700e25bf05a625faf7d2c9d2710f332db79
adoniasvsbarros <adoniasvicente@hotmail.com>
2019-08-17 14:49:56 -0300

    cross-component commun. in recipe service

--- basic-project/src/app/recipes/recipe.service.ts
+++ basic-project/src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
 import { Recipe } from './recipe.module';
+import { EventEmitter } from '@angular/core';
 
 export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
     private recipes: Recipe[] = [
         new Recipe('Test Recipe', 'This is a simply test', 'https://www.wellplated.com/wp-content/uploads/2017/12/Hoppin-John-recipe-600x629.jpg'),
         new Recipe('Test Recipe', 'This is a simply test', 'https://www.wellplated.com/wp-content/uploads/2017/12/Hoppin-John-recipe-600x629.jpg'),
